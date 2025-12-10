f72b28c82738399bba815d696d968b1adedde500
Praneel Patel <tlm.dev.BR@gmail.com>
2025-12-10 17:57:48 -0600

    Replace gyro object with a Pigeon2

--- src/main/java/frc/robot/subsystems/swerve/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveDrive.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems.swerve;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -14,8 +17,6 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.swerve.constants.SwerveConstants.DriveConstants;
 
@@ -42,12 +43,12 @@ public class SwerveDrive extends SubsystemBase {
 		DriveConstants.kBackRightChassisAngularOffset);
 
 	// The gyro sensor
-	private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
+	private final Pigeon2 gyro = new Pigeon2(30);
 
 	// Odometry class for tracking robot pose
 	SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
 		DriveConstants.kDriveKinematics,
-		Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
+		Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble()),
 		new SwerveModulePosition[] {
 			m_frontLeft.getPosition(),
 			m_frontRight.getPosition(),
@@ -65,7 +66,7 @@ public class SwerveDrive extends SubsystemBase {
 	public void periodic() {
 		// Update the odometry in the periodic block
 		m_odometry.update(
-			Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
+			Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble()),
 			new SwerveModulePosition[] {
 				m_frontLeft.getPosition(),
 				m_frontRight.getPosition(),
@@ -90,7 +91,7 @@ public class SwerveDrive extends SubsystemBase {
 	 */
 	public void resetOdometry(Pose2d pose) {
 		m_odometry.resetPosition(
-			Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
+			Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble()),
 			new SwerveModulePosition[] {
 				m_frontLeft.getPosition(),
 				m_frontRight.getPosition(),
@@ -118,7 +119,7 @@ public class SwerveDrive extends SubsystemBase {
 		var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
 			fieldRelative
 				? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
-					Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)))
+					Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble()))
 				: new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
 		SwerveDriveKinematics.desaturateWheelSpeeds(
 			swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
@@ -162,7 +163,7 @@ public class SwerveDrive extends SubsystemBase {
 
 	/** Zeroes the heading of the robot. */
 	public void zeroHeading() {
-		m_gyro.reset();
+		gyro.reset();
 	}
 
 	/**
@@ -171,7 +172,7 @@ public class SwerveDrive extends SubsystemBase {
 	 * @return the robot's heading in degrees, from -180 to 180
 	 */
 	public double getHeading() {
-		return Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)).getDegrees();
+		return Rotation2d.fromDegrees(gyro.getYaw().getValueAsDouble()).getDegrees();
 	}
 
 	/**
@@ -180,6 +181,6 @@ public class SwerveDrive extends SubsystemBase {
 	 * @return The turn rate of the robot, in degrees per second
 	 */
 	public double getTurnRate() {
-		return m_gyro.getRate(IMUAxis.kZ) * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
+		return gyro.getAngularVelocityZDevice().getValueAsDouble() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
 	}
 }
